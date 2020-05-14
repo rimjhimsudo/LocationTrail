abdebb763e40b49cd11df6ac5329d3ca359b8925
rimjhimsudo <rimjhimvaish@gmail.com>
2020-05-14 14:20:58 +0530

    arraylst added

--- app/src/main/java/com/example/locationtrailapp/MainActivity.java
+++ app/src/main/java/com/example/locationtrailapp/MainActivity.java
@@ -6,17 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,12 +22,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity  {
     private Button btn_requestloc;
-    private TextView tlat , tlong , tcountry;
+    private TextView tlat , tlong , tcountry,tlaytfromarray;
+    //int intArray[];
+    private ArrayList<String> latlongList = new ArrayList<String>();
+    int j=0;
     //private LocationManager locationManager;
    // private LocationListener listener;
     FusedLocationProviderClient fusedLocationProviderClient;
@@ -47,6 +45,7 @@ public class MainActivity extends AppCompatActivity  {
         tlat =  findViewById(R.id.textv1);
         tlong= findViewById(R.id.textv2);
         tcountry= findViewById(R.id.textv3);
+        tlaytfromarray= findViewById(R.id.textv4);
         btn_requestloc = findViewById(R.id.b1);
         fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
         btn_requestloc.setOnClickListener(new View.OnClickListener() {
@@ -80,9 +79,18 @@ public class MainActivity extends AppCompatActivity  {
                         List<Address> address= geocoder.getFromLocation(
                                 location.getLatitude(),location.getLongitude(),1
                         );
+                     //   intArray[j]= (int) address.get(0).getLatitude();
+                        String lat=String.valueOf( address.get(0).getLatitude());
+                        String lon=String.valueOf( address.get(0).getLongitude());
+                        String latlong=lat +" . " +lon;
+                        latlongList.add(latlong);
+                        Log.d("TAGVALUEs","latlon"+latlong);
+
                         tlat.setText(String.valueOf( address.get(0).getLatitude()));
                         tlong.setText(String.valueOf( address.get(0).getLongitude()));
                         tcountry.setText(String.valueOf( address.get(0).getCountryName()));
+                        //String.valueOf(address.get(0).);
+
 
                     }
                     catch (IOException excptn){
